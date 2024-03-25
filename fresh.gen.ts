a619b4e7abf9399d17c6c2899ad2f30919f4dd98
Jack <i@jackpan.me>
2024-03-25 14:53:10 +0800

    chore: upgrade fresh to 1.6.8

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -3,6 +3,7 @@
 // This file is automatically updated during development when running `dev.ts`.
 
 import * as $_404 from "./routes/_404.ts";
+import * as $_app from "./routes/_app.tsx";
 import * as $_middleware from "./routes/_middleware.ts";
 import * as $index from "./routes/index.ts";
 import * as $internal_middleware from "./routes/internal/_middleware.ts";
@@ -31,6 +32,7 @@ import { type Manifest } from "$fresh/server.ts";
 const manifest = {
   routes: {
     "./routes/_404.ts": $_404,
+    "./routes/_app.tsx": $_app,
     "./routes/_middleware.ts": $_middleware,
     "./routes/index.ts": $index,
     "./routes/internal/_middleware.ts": $internal_middleware,
